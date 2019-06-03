99894f77a08123f3c47b9a9daf8a3d0acbb221b1
home <orcasuit@gmail.com>
2019-06-04 01:55:11 +0900

    도뎇.ㅔ...왜 ... 안도는...건...데..........아아아ㅏ아ㅏ아아아아ㅏ아아아아ㅏㄱ

--- app/src/main/java/com/example/dojeon_part06_m1/MainActivity.java
+++ app/src/main/java/com/example/dojeon_part06_m1/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.dojeon_part06_m1;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolBar;
+    CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setTitle(" ");
     }
 }
